b7da17eae3ab8d9d25ba016c620e731ddd0a01b8
joranegs92 <joranegs92@gmail.com>
2022-07-19 22:10:02 +0900

    vue setting 추가

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,12 @@
-const { defineConfig } = require('@vue/cli-service')
-module.exports = defineConfig({
-  transpileDependencies: true
-})
+
+module.exports = {
+  outputDir: '../src/main/resources/static',
+  devServer: {
+    port: 3030,
+    proxy: {
+      '/api': {
+        target: 'http://localhost:3000'
+      }
+    }
+  }
+}
