64588af7b6b05ec97a4ee118b4f4b953c4a64351
joranegs92 <joranegs92@gmail.com>
2022-07-19 22:24:00 +0900

    axios와 연결

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -5,7 +5,7 @@
       <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
     </head>
     <body>
-    <p >안녕하세요 </p>
+    <button @click="save()" >안녕하세요 </button>
     </body>
   </div>
 </template>
